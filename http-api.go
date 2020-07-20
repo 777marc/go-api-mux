901f5b886ffa52a557dc87288a43267a52232c5d
777marc@gmail.com <777marc@gmail.com>
2020-07-20 18:53:47 -0400

    misc

--- http-api.go
+++ http-api.go
@@ -7,10 +7,17 @@ import (
 	"github.com/gorilla/mux"
 )
 
+var data []string = []string{}
+
 func main() {
+
 	router := mux.NewRouter()
+
 	router.HandleFunc("/test", test)
+	router.HandleFunc("/add/{item}", addItem)
+
 	http.ListenAndServe(":5000", router)
+
 }
 
 func test(w http.ResponseWriter, r *http.Request) {
@@ -19,6 +26,13 @@ func test(w http.ResponseWriter, r *http.Request) {
 
 	json.NewEncoder(w).Encode(struct {
 		ID string
-	}{"mystringid777"})
+	}{"777"})
+
+}
+
+func addItem(w http.ResponseWriter, r *http.Request) {
+
+	item := mux.Vars(r)["item"]
+	data = append(data, item)
 
 }
